358af21ce21be745e86cb31b8f54b27bf2c62b88
alexcanessa <canessa.alex@gmail.com>
2024-12-23 13:31:09 +0100

    fix: remove extra bottom scroll

--- src/components/ChoiceTable/index.tsx
+++ src/components/ChoiceTable/index.tsx
@@ -12,7 +12,7 @@ const ChoiceTable = ({
       <div className="px-5 flex justify-around align-middle gap-10">
         {children}
       </div>
-      <div className="absolute z-10 bottom-0 flex justify-center items-center bg-green-600 h-[200px] w-full border-b-green-800 border-b-[20px]">
+      <div className="absolute overflow-hidden z-10 bottom-0 flex justify-center items-center bg-green-600 h-[200px] w-full border-b-green-800 border-b-[20px]">
         <div className="absolute top-1/4 z-20 -bottom-14 w-full bg-white blur-xs opacity-20 rounded-[50%]"></div>
       </div>
     </div>
